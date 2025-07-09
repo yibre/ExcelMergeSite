906d0e202760a6b944f0b5f55b12d8643318268e
yibre <cdoyue@gmail.com>
2025-07-10 04:21:01 +0900

    [FEAT] installed multipart dependancy

--- routers/api.py
+++ routers/api.py
@@ -1,22 +1,27 @@
+import os
+import shutil
+
 from typing import Annotated
 from fastapi.templating import Jinja2Templates
 from fastapi import APIRouter, File, UploadFile, Request
 from fastapi.responses import HTMLResponse
 
+# API router 세팅 및 html template 연결
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-"""
-@router.post("/files")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+# Configuration
+UPLOADS_DIR = "uploads"
 
-@router.post("/uploadfile")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
-"""
+# Upload directory가 없으면 만들기
+os.makedirs(UPLOADS_DIR, exist_ok=True)
 
 # request는 Jinja2에서 늘 들어가야함
 @router.get("/test", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("world.html", {"request": request, "message": "Hello, World!"})
+
+
+@router.get("/main", response_class=HTMLResponse)
+async def read_home(request : Request):
+    pass
